7dd493ef9eefb2a1bd2ce10af54b43f2f0def1e7
Sidney Shafer <sidney@mnshafers.com>
2024-02-05 10:29:38 -0600

    Add project files.

---
+++ calculator.forms/calculator.forms/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace calculator.forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+
+        private void BtnOff_Clicked(object sender, EventArgs e)
+        {
+            LblResult.BindingContext = string.Empty;
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            var newViewModelInstance = new MainPageViewModel();
+            BindingContext = newViewModelInstance;
+
+        }
+
+        private void BtnOn_Clicked(object sender, EventArgs e)
+        {
+            var newViewModelInstance = new MainPageViewModel();
+            BindingContext = newViewModelInstance;
+
+            LblResult.BindingContext = newViewModelInstance;
+        }
+    }
+}
---
+++ calculator.forms/calculator.forms/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using calculator.core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace calculator.forms
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private Calculator _calculator;
+
+        private string result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            _calculator = new Calculator();
+            result = "0";
+
+            PushNumberCommand = new Command<string>(_ => _calculator.PushNumber(_));
+            PushOperationCommand = new Command<string>(_ => _calculator.PushOperation(_[0]));
+            _calculator.ResultChanged = _ => { Result = _; };
+        }
+
+        public Command<string> PushNumberCommand { get; set; }
+
+        public Command<string> PushOperationCommand { get; set; }
+
+        public string Result
+        {
+            get => result;
+            set
+            {
+                result = value;
+                PropertyChanged?.Invoke(this, 
+                    new PropertyChangedEventArgs(nameof(Result)));
+            }
+        }
+    }
+}
